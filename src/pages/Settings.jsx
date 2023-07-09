d62dc3e1d8902dc18936a8944d50b73df9fd650e
Letizia <116899981+letizia-sorrentino@users.noreply.github.com>
2023-07-09 22:20:21 +0100

    added link to settings

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,11 +1,17 @@
+import { Link } from "react-router-dom";
+import "../styles/settings.css"
+
 const Settings = () => {
   return (
     <>
-      <div>
+      <div className="settingsContainer">
         <h1>Settings</h1>
-        <h2>Account</h2>
+        <div>
+          <Link className="settingsLink" to={"/createAccount"}>
+            <p>Create an Account</p>
+          </Link>
+        </div>
       </div>
-    
     </>
   );
 };
