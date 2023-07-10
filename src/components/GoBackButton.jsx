9bb9acb9d620d9c0273fbed183058030fb25f561
Letizia <116899981+letizia-sorrentino@users.noreply.github.com>
2023-07-10 20:44:11 +0100

    added new favicon

---
+++ src/components/GoBackButton.jsx
@@ -0,0 +1,15 @@
+// import { useNavigate } from "react-router-dom";
+
+// const GoBackButton = () => {
+// const navigate = useNavigate();
+// return (
+
+//     <div>
+//         <button onClick={navigate(-1)}>Go Back</button>
+//     </div>
+
+
+// )
+// }
+
+// export default GoBackButton;
--- src/pages/CreateAccountPage.jsx
+++ src/pages/CreateAccountPage.jsx
@@ -1,13 +1,10 @@
-import AccountForm from '../features/account/AccountForm';
+import AccountForm from "../features/account/AccountForm";
 
 const CreateAccountPage = () => {
-
-
-return (
+  return (
     <div>
-    <AccountForm/>
+      <AccountForm />
     </div>
-)
-
+  );
 };
 export default CreateAccountPage;
