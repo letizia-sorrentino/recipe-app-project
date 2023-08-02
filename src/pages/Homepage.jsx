a810de980b0683e6a5c39ae83e14f7df0ff4200f
Letizia <116899981+letizia-sorrentino@users.noreply.github.com>
2023-08-02 07:16:53 +0100

    changed title

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -6,7 +6,7 @@ import pescatarianDish from "../assets/pescatarianDish.png";
 import whole30Dish from "../assets/Whole30Dish.png";
 import { getRecipesByDiet } from "../features/recipes/recipeAPI";
 import { Link } from "react-router-dom";
-import "../styles/homepage.css"
+import "../styles/homepage.css";
 
 const Homepage = () => {
   return (
@@ -24,91 +24,91 @@ const Homepage = () => {
       <div>
         <h2>Popular Recipes</h2>
         <div className="categoriesContainer">
-        <div className="categoryCard">
+          <div className="categoryCard">
             <Link className="categoryLink" to={"/searchResults"}>
-              <div> 
-              <img
-                className="categoryPicture"
-                src={vegetarianDish}
-                alt="vegetarianDish"
-              />
+              <div>
+                <img
+                  className="categoryPicture"
+                  src={vegetarianDish}
+                  alt="vegetarianDish"
+                />
               </div>
-              <div> 
-              <button
-                className="categoryTitle"
-                onClick={() => {
-                  getRecipesByDiet("vegetarian");
-                  console.log("vegetarian recipes");
-                }}
-              >
-                Vegeterian
-              </button>
+              <div>
+                <button
+                  className="categoryTitle"
+                  onClick={() => {
+                    getRecipesByDiet("vegetarian");
+                    console.log("vegetarian recipes");
+                  }}
+                >
+                  Vegeterian
+                </button>
               </div>
             </Link>
           </div>
           <div className="categoryCard">
             <Link className="categoryLink" to={"/searchResults"}>
-              <div> 
-              <img
-                className="categoryPicture"
-                src={veganDish}
-                alt="veganDish"
-              />
+              <div>
+                <img
+                  className="categoryPicture"
+                  src={veganDish}
+                  alt="veganDish"
+                />
               </div>
-              <div> 
-              <button
-                className="categoryTitle"
-                onClick={() => {
-                  getRecipesByDiet("vegan");
-                  console.log("vegan recipes");
-                }}
-              >
-                Vegan
-              </button>
+              <div>
+                <button
+                  className="categoryTitle"
+                  onClick={() => {
+                    getRecipesByDiet("vegan");
+                    console.log("vegan recipes");
+                  }}
+                >
+                  Vegan
+                </button>
               </div>
             </Link>
           </div>
           <div className="categoryCard">
-                        <Link className="categoryLink" to={"/searchResults"}>
-              <div> 
-              <img
-                className="categoryPicture"
-                src={pescatarianDish}
-                alt="pescatarianDish"
-              />
+            <Link className="categoryLink" to={"/searchResults"}>
+              <div>
+                <img
+                  className="categoryPicture"
+                  src={pescatarianDish}
+                  alt="pescatarianDish"
+                />
               </div>
-              <div> 
-              <button
-                className="categoryTitle"
-                onClick={() => {
-                  getRecipesByDiet("pescatarian");
-                  console.log('pescatarian recipes')
-                }}
-              >
-                Pescatarian{" "}
-              </button>
+              <div>
+                <button
+                  className="categoryTitle"
+                  onClick={() => {
+                    getRecipesByDiet("pescatarian");
+                    console.log("pescatarian recipes");
+                  }}
+                >
+                  Pescatarian{" "}
+                </button>
               </div>
             </Link>{" "}
           </div>
           <div className="categoryCard">
-                        <Link className="categoryLink" to={"/searchResults"}>
-              <div> 
-              <img
-                className="categoryPicture"
-                src={whole30Dish}
-                alt="whole30Dish"
-              />
+            <Link className="categoryLink" to={"/searchResults"}>
+              <div>
+                <img
+                  className="categoryPicture"
+                  src={whole30Dish}
+                  alt="whole30Dish"
+                />
               </div>
-              <div> 
-              <button
-                className="categoryTitle"
-                onClick={() => {
-                  getRecipesByDiet("whole30");
-                  console.log('whole30')
-                }}
-              >
-                Whole30
-              </button>
+              <div>
+                <button
+                  className="categoryTitle"
+                  onClick={() => {
+                    getRecipesByDiet("whole30");
+                    console.log("whole30");
+                  }}
+                >
+                  Whole30
+                </button>
               </div>
             </Link>{" "}
           </div>
